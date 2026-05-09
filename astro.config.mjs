53f5f4c231e49edd189820067f44ba67812f9aff
Edmund Harriss <eoharriss@gmail.com>
2026-05-09 14:32:59 +0200

    Will it build?

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,12 +11,12 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			customCss: ['katex/dist/katex.min.css'],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
